afe9dc5e4105d07816cb792ce4e6e0c0ec081a78
Matheus Cristian <matheuscristianrg@gmail.com>
2025-02-17 22:08:29 -0300

    Create the base code for handling the appointment requests

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const path = require("path");
+const util = require("util");
+const bodyParser = require("body-parser");
 
 const db = require("./database.js");
 
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "static/")));
 
 // Define o sistema de renderização dinâmica
@@ -22,6 +25,14 @@ app.get("/sobre", (req, res) => {
 
 app.get("/agendar", async (req, res) => {
 	res.render("agendamento", { services: await db.Services.getServices(), appointments: await db.Appointments.getAppointments() });
+});
+
+// Recebe as informações dos agendamentos
+app.post("/agendar", (req, res) => {
+	console.log(util.styleText("yellow", "INFO: ") + "Recebendo informações POST de '/agendar'...");
+	console.log(req.body);
+	
+	res.redirect("/agendar");
 });
 
 app.listen(port, () => {
